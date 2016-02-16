490e49981e1fbc4f4f660487428e705dbcd06c6b
budak <thatbudakguy@gmail.com>
2016-02-16 14:48:43 -0800

    add welcome page
    - add style.css emulating library main page
    - add dummy images to represent sr art contributions
    - three login portals

---
+++ submit_ui/welcome.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>LC Senior Projects</title>
+    <!-- Bootstrap CDN imports -->
+    <!-- JQuery (required for bootstrap) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <!-- Style for this page -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <!-- NAVBAR -->
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand">Welcome</a>
+        </div>
+      </div>
+    </nav>
+
+    <!-- MAIN -->
+
+    <div class="container-fluid">
+      <div class="main">
+
+        <div id="all" class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+          <div class="col-md-4">
+            <a href="#"><h2 class="role" >Students</h2></a>
+            <p>Submit a project for review and publishing.</p>
+            <p><a id="login-student" class="btn btn-primary btn-sm" href="#" role="button">Login &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <a href="#"><h2 class="role" >Faculty</h2></a>
+            <p>Review and approve or deny student projects.</p>
+            <p><a id="login-student" class="btn btn-primary btn-sm" href="#" role="button">Login &raquo;</a></p>
+         </div>
+          <div class="col-md-4">
+            <a href="#"><h2 class="role">Staff</h2></a>
+            <p>Manage the review and publishing process.</p>
+            <p><a id="login-student" class="btn btn-primary btn-sm" href="#" role="button">Login &raquo;</a></p>
+          </div>
+        </div>
+        </div>
+
+          <!-- JUMBOTRON -->
+
+          <!-- Main jumbotron for a primary marketing message or call to action -->
+          <div class="jumbotron">
+            <div class="container">
+              <h1 class="display-3">Senior Projects at Lewis & Clark</h1>
+              <p>A digital archive and showcase of senior theses, projects, and presentations by Lewis & Clark students.</p>
+              <p><a id="go" class="btn btn-primary btn-lg" href="http://52.89.97.243" role="button">Visit the site &raquo;</a></p>
+            </div>
+            <div class="container">
+              <div class="row">
+                <div class="col-md-4 col-sm-6">
+                  <a href="#" class="thumbnail"><img src="t1.jpg" /></a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                  <a href="#" class="thumbnail"><img src="t2.jpg" /></a>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                  <a href="#" class="thumbnail"><img src="t3.jpg" /></a>
+                </div>
+            </div>
+          </div>
+          <hr>
+          <footer class="footer">
+            <p>&copy; LC Digital Initiatives 2016</p>
+          </footer>
+        </div>
+
+
+      </div>
+    </div>
+  </body>
+</html>
