8ac166d7e202350d4777892d8fda151b551a8b04
budak <thatbudakguy@gmail.com>
2016-02-18 10:46:37 -0800

    Revert "authentication update v4 :thumb:"
    This reverts commit 030d39135819b2f7f82a3c7ff6d02ae336ce722c.

--- submit_ui/global.js
+++ submit_ui/global.js
@@ -1,18 +1,16 @@
 $( document ).ready(function() {
 
   // Login modal
-  $('#loginModal').on('shown.bs.modal', function (event) {
-
-    if ($('.alert-danger').length){ // get rid of previous error messages in this modal if any are lurking - clean slate!
-      $('.alert-danger').remove();
-    }
-
+  $('#loginModal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget); // Find the button that triggered the modal
     var role = button.data('role'); // Extract info about what kind of patron logged in from button's data-* attribute
     $('#username').focus(); // Move the cursor to the first field automatically
 
-    var checkAuth = function(role,username,password){
-      $.post("auth.php", { role:role, username:username, password:password },
+    // Make the submit button trigger a POST request to authenticate with LDAP
+    $('#loginbtn').click({role:role},function(e){
+      username = $('#username').val();
+      password = $('#password').val();
+      $.post("auth.php", { role:e.data.role, username:username, password:password },
         function(json){
           result = $.parseJSON(json);
           if (result.success == true){
@@ -32,29 +30,13 @@ $( document ).ready(function() {
                 break;
             }
             window.location.assign(url);
+
           }
           else if (!$('.alert-danger').length){
             $('.modal-body').prepend("<div class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span><span class='sr-only'>Error: incorrect login </span> "+result.msg+"</div>");
           }
         });
-    }
-
-    // Make the submit button trigger a POST request to authenticate with LDAP
-    $('#loginbtn').click({role:role},function(){
-      username = $('#username').val();
-      password = $('#password').val();
-      checkAuth(role,username,password);
     });
-
-    // Also bind the enter key to do the same thing
-    $("input").keypress({role:role},function(event) {
-      if (event.which == 13){
-        username = $('#username').val();
-        password = $('#password').val();
-        checkAuth(role,username,password);
-      }
-    });
-
   });
 
   // Logout function
